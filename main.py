69918776fb0694d5805e7ac59cd57fab9bd2fc1e
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2023-11-20 23:59:25 +0500

    add code of generating complete graph

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
 import tkinter as tk
+import math
 
 class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def draw_point(event):
+    global points
+    global selectedValue
+    x = event.x
+    y = event.y
+    print(selectedValue)
+    if(selectedValue == 1):
+        points.append(Point(x, y))
+        canvas.create_oval(x-4, y-4, x+4, y+4, fill="red")
+
+def on_radio_select():
+    global selectedValue
+    selectedValue = radio_var.get()
+
+def update_label(value):
+    global sliderValue
+    canvas.delete("all")
+    sliderValue = int(sliderValue) - 1
+
+    radius = 100
+    center_x, center_y = 200, 150
+    angle_increment = 360 / sliderValue
+
+    vertices = []
+    for i in range(sliderValue):
+        angle_rad = i * angle_increment
+        x = center_x + radius * math.cos(math.radians(angle_rad))
+        y = center_y + radius * math.sin(math.radians(angle_rad))
+        vertices.append((x, y))
+        canvas.create_oval(x - 5, y - 5, x + 5, y + 5, fill="red")
+
+    for i in range(sliderValue):
+        for j in range(i + 1, sliderValue):
+            x1, y1 = vertices[i]
+            x2, y2 = vertices[j]
+            canvas.create_line(x1, y1, x2, y2, fill="blue")
+    sliderValue = value
 
 def clear_content():
     label.destroy()
@@ -18,34 +60,57 @@ def simpleGraph():
 
     label1 = tk.Label(app, text="Simple Graph")
     label1.grid(row=0, column=0, padx=10, pady=10)
-    canvas = tk.Canvas(app, width=500, height=500, bg="lightgray")
-    canvas.bind("<Button-1>", on_canvas_click)
-    canvas.grid(row=2, column=0, padx=10, pady=10)
+    radio_button1.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+    radio_button2.grid(row=2, column=1, padx=10, pady=10, sticky="w")
+    canvas.bind("<Button-1>", draw_point)
+    canvas.grid(row=3, column=0, padx=10, pady=10)
     app.columnconfigure(0, weight=1)
     app.rowconfigure(2, weight=1)
 
-def on_button_click():
-    label.config(text="Button Clicked!")
 
-def on_canvas_click(event):
-    x = event.x
-    y = event.y
-    print(f"Clicked at coordinates: ({x}, {y})")
+def completeGraph():
+    label.destroy()
+    btn1.destroy()
+    btn2.destroy()
+    btn3.destroy()
+    btn4.destroy()
 
+    label1 = tk.Label(app, text="Complete Graph")
+    label1.grid(row=0, column=0, padx=10, pady=10)
+    slider.grid(row=1, column=0, padx=10, pady=10)
+    canvas.bind("<Button-1>", draw_point)
+    canvas.grid(row=3, column=0, padx=10, pady=10)
+    app.columnconfigure(0, weight=1)
+    app.rowconfigure(2, weight=1)
+
+    
+
+points = []
 app = tk.Tk()
 app.geometry("800x600")
 
 app.title("Graph Theory Project")
 
 label = tk.Label(app, text="Main Page")
-
+selectedValue = -1
+radio_var = tk.IntVar()
 btn1 = tk.Button(app, text="Draw Simple Graph", command=simpleGraph)
-btn2 = tk.Button(app, text="Generate Complete Graph", command=clear_content)
+btn2 = tk.Button(app, text="Generate Complete Graph", command=completeGraph)
 btn3 = tk.Button(app, text="Bipartite and Tripartite Graph", command=clear_content)
 btn4 = tk.Button(app, text="Havel Hakimi", command=clear_content)
+radio_button1 = tk.Radiobutton(app, text="Add Vertices", variable=radio_var, value=1, command=on_radio_select)
+radio_button2 = tk.Radiobutton(app, text="Add Edges", variable=radio_var, value=2, command=on_radio_select)
+canvas = tk.Canvas(app, width=500, height=500, bg="lightgray")
+slider = tk.Scale(
+    app,
+    from_=10,
+    to=100,
+    orient=tk.HORIZONTAL,
+    length=200,
+    command=update_label
+)
+sliderValue = 10
 
-# canvas = tk.Canvas(app, width=400, height=400, bg="lightgray")
-# canvas.bind("<Button-1>", on_canvas_click)
 
 label.grid(row=0, column=0, padx=10, pady=10)
 btn1.grid(row=1, column=0, padx=10, pady=10, columnspan=2)
